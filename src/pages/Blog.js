41c109ee2aedd164b04e1db20b18e35fc1fa69b7
Talha <mail.talha.pro@gmail.com>
2020-07-20 18:22:45 +0500

    Updated for deployment

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,1 +1,11 @@
 import React from "react"
+
+const Blog = () => {
+  return (
+    <section>
+      <h1>This is blog</h1>
+    </section>
+  )
+}
+
+export default Blog
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,1 +1,11 @@
 import React from "react"
+
+const Store = () => {
+  return (
+    <section>
+      <h1>This is Store</h1>
+    </section>
+  )
+}
+
+export default Store
