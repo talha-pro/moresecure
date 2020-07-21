63a6f8c6feecfab5f5613f9803335d8dc2e7f3bb
Talha <mail.talha.pro@gmail.com>
2020-07-21 18:00:07 +0500

    About Page Header Component Added

--- src/pages/About.js
+++ src/pages/About.js
@@ -30,11 +30,42 @@ import arcsight from "../assets/About/arcsight.png"
 import enterasys from "../assets/About/enterasys.jpeg"
 import altor from "../assets/About/altor.png"
 import art from "../assets/About/art.png"
+import KeepInformed from "../components/KeepInformed"
 
 const About = () => {
   return (
     <>
       <NavBar />
+      <header className="header">
+        <Container className="header__container">
+          <Row className="header__row">
+            <Col className="header__left-col">
+              <div className="header-wrapper">
+                <div className="main-sub-heading">
+                  Grow Your Business and Risk Less with
+                </div>
+                <div className="main-heading">#MoreSecure</div>
+                <KeepInformed />
+              </div>
+            </Col>
+            <Col className="header__right-col"></Col>
+          </Row>
+          <div className="space-4"></div>
+          <div className="space-4"></div>
+          <div className="space-4"></div>
+        </Container>
+        <svg
+          className="header__svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#fff"
+            fill-opacity="1"
+            d="M0,96L60,90.7C120,85,240,75,360,90.7C480,107,600,149,720,149.3C840,149,960,107,1080,101.3C1200,96,1320,128,1380,144L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
+      </header>
       <section className="about">
         <Container className="about__container">
           <Row className="about__row">
